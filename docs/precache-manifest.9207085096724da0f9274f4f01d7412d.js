67478c07e9188f4edf256bf387ffa43524f9dcd4
Julie Berteloot <julie.berteloot.fr@gmail.com>
2019-08-21 14:06:02 -0700

    added docs

---
+++ docs/precache-manifest.9207085096724da0f9274f4f01d7412d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1cb63ac6eb1c13fb6773f35be546153",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bfebe902018cca2194f",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "28beca847ac21672bc46",
+    "url": "/static/css/main.86deaee0.chunk.css"
+  },
+  {
+    "revision": "4bfebe902018cca2194f",
+    "url": "/static/js/2.219baa8e.chunk.js"
+  },
+  {
+    "revision": "28beca847ac21672bc46",
+    "url": "/static/js/main.316682d3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
